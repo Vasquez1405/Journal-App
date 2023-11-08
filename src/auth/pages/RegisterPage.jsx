4748d2e55877f629b81ab772ca6190b3739dfcd2
Oscar Vasquez <oscareduc14@hotmail.com>
2023-11-08 15:07:35 -0500

    Creacion del diseño de la app

---
+++ src/auth/pages/RegisterPage.jsx
@@ -0,0 +1,40 @@
+import { Link as RouterLink } from 'react-router-dom'
+import { Google } from '@mui/icons-material'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { AuthLayout } from '../layout/AuthLayout'
+
+export const RegisterPage = () => {
+    return (
+        <AuthLayout title='Crear Cuenta'>
+            <form>
+
+                <Grid container>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField label='Nombre completo' type='texto' placeholder='Nombre completo' fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField label='Correo' type='email' placeholder='correo@google.com' fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField label='Contraseña' type='password' placeholder='Contraseña' fullWidth />
+                    </Grid>
+
+                    <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+                        <Grid item xs={12} sm={6}>
+                            <Button variant='contained' fullWidth >
+                                Crear cuenta
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+                <Grid container direction='row' justifyContent='end'>
+                    <Link component={RouterLink} color='inherit' to='/auth/login' >
+                        Ya tienes cuenta?
+                    </Link>
+                </Grid>
+
+            </form>
+        </AuthLayout>
+    )
+}
